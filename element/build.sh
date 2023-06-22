3028db26736cce9a6ccc8abd8868a98887aaadc4
Remi Rampin <remi@rampin.org>
2023-06-21 22:56:54 -0400

    Update Element to 1.11.34

--- element/build.sh
+++ element/build.sh
@@ -2,4 +2,4 @@
 docker buildx build --pull \
     . \
     --platform linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64,linux/ppc64le \
-    --push --tag quay.io/remram44/element:1.11.33
+    --push --tag quay.io/remram44/element:1.11.34
