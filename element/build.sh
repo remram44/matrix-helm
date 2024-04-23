69f51f945a2cfb39f62aa6843a48051e77cc4dec
Remi Rampin <remi@rampin.org>
2024-04-23 14:11:00 -0400

    Remove arm/v5, not available for nginx 1.25.5

--- element/build.sh
+++ element/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker buildx build --pull \
     . \
-    --platform linux/amd64,linux/arm/v5,linux/arm/v7,linux/arm64,linux/ppc64le \
+    --platform linux/amd64,linux/arm/v7,linux/arm64,linux/ppc64le \
     --push --tag ghcr.io/remram44/matrix-helm/element:1.11.65
