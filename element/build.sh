54036fff5464d8b4261684a9f5c6b32400cda2a0
Remi Rampin <remi@rampin.org>
2024-12-03 11:27:57 -0500

    Update Element to 1.11.87

--- element/build.sh
+++ element/build.sh
@@ -2,4 +2,4 @@
 docker buildx build --pull \
     . \
     --platform linux/amd64,linux/arm/v7,linux/arm64,linux/ppc64le \
-    --push --tag ghcr.io/remram44/matrix-helm/element:1.11.86
+    --push --tag ghcr.io/remram44/matrix-helm/element:1.11.87
